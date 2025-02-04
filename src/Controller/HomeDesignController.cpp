1ffe66108e2867e8e9296f662689cc98758447d2
Ahmed ibarhim <aibrahim@tmcmf.com>
2025-02-04 06:30:09 +0200

    dd

--- src/Controller/HomeDesignController.cpp
+++ src/Controller/HomeDesignController.cpp
@@ -163,7 +163,7 @@ DesignRepository HomeDesignController::extractRealTemplateDesign()
     for(auto &template1 : templatesDesignsData)
     {
         index++;
-        if(index > 1)continue;
+        if(index > 2)continue;
         DesignGenerator designGenerator1;
         vector<Design>ds = designGenerator1.generateDesign(template1);
 
