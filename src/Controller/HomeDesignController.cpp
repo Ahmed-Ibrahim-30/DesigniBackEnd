c4e2e533519778194e7fe8daa4f0cee85f33ca47
Ahmed ibarhim <aibrahim@tmcmf.com>
2025-02-04 05:46:40 +0200

    up

--- src/Controller/HomeDesignController.cpp
+++ src/Controller/HomeDesignController.cpp
@@ -1430,6 +1430,33 @@ void HomeDesignController::getUserInput() {
         return finalRes;
     });
 
+    CROW_ROUTE(app , "/GETFirstName").methods(crow::HTTPMethod::POST)([&](const crow::request&req)
+    {
+        auto jsonData = crow::json::load(req.body);
+//        std::cout << "Request Body: " << req.body << std::endl;
+    if (!jsonData) {
+        return crow::response(400, "Invalid JSON format");
+    }
+
+    string name;
+    if(jsonData.has("name"))
+    {
+        name = jsonData["name"].s();
+    }
+    crow::json::wvalue response;
+    response["Name"] = "Your name = " + name;
+    crow::response finalRes (200 , response);
+    finalRes.add_header("Access-Control-Allow-Origin", "*");
+    finalRes.add_header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    finalRes.add_header("Access-Control-Allow-Headers", "Content-Type");
+    return finalRes;});
+
+    app.port(8080).multithreaded().run();
+
+    cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
     app.port(8080).multithreaded().run();
 }
 
