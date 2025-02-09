e77adca9d811c3fe656f262b647a480567a27d0e
Ahmed ibarhim <aibrahim@tmcmf.com>
2025-02-09 11:52:21 +0200

    up

--- src/Model/Land/Green Area/CentralLandGreenSelector.cpp
+++ src/Model/Land/Green Area/CentralLandGreenSelector.cpp
@@ -79,13 +79,13 @@ void CentralLandGreenSelector::select(Polygon1 &outerLand, vector<Polygon1> &lan
     if (allGreenAreasOptions.empty()) return;
     std::sort(allGreenAreasOptions.begin(), allGreenAreasOptions.end());
 
-    int solIndex = lower_bound(allGreenAreasOptions.begin(), allGreenAreasOptions.end(),totalGreenAreas ,
-                               [](pair<double, vector<int>> &elem , double value)
-                               {
-        return elem.first < value;
-                               })-allGreenAreasOptions.begin();
+//    int solIndex = lower_bound(allGreenAreasOptions.begin(), allGreenAreasOptions.end(),totalGreenAreas ,
+//                               [](pair<double, vector<int>> &elem , double value)
+//                               {
+//        return elem.first < value;
+//                               })-allGreenAreasOptions.begin();
 
-    vector<int> greenAreaSelected = allGreenAreasOptions[solIndex].second;
+    vector<int> greenAreaSelected = allGreenAreasOptions[0].second;
 
     for(auto &green : greenAreaSelected)
     {
