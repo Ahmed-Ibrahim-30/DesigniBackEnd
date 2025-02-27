5f0c0535cbc6c0adee7bf3d1abf4e466587e4b52
Ahmed ibarhim <aibrahim@tmcmf.com>
2025-02-27 05:37:36 +0200

    ff

--- src/Model/Land/LandDivisionRoads/LandDivisionRoadsByDivisionsCount.cpp
+++ src/Model/Land/LandDivisionRoads/LandDivisionRoadsByDivisionsCount.cpp
@@ -77,7 +77,7 @@ void LandDivisionRoadsByDivisionsCount::divideLand(double ratioA, double ratioB,
 
         auto polygonDivided = pols[i];
 
-        vector<pair<Polygon1 , Polygon1>> paiPoly = dividePolygons( polygonDivided , mainLand.getArea() / 500000);
+        vector<pair<Polygon1 , Polygon1>> paiPoly = dividePolygons( polygonDivided , mainLand.getArea() * (0.01/100));
 
         for (const auto& div : paiPoly)
         {
