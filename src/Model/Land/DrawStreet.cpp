5a00781fdab0eb58e4562639150e82ece8abbd30
Ahmed ibarhim <aibrahim@tmcmf.com>
2025-03-09 16:16:42 +0200

    debug

--- src/Model/Land/DrawStreet.cpp
+++ src/Model/Land/DrawStreet.cpp
@@ -130,7 +130,6 @@ void DrawStreet::drawStreets(Polygon1 &polygon1)
         homeLines.emplace_back(next1UP.getX() , next1UP.getY() , next2UP.getX() , next2UP.getY());
 
         streets.push_back(homeLines);
-
     }
 
 //    for (int i = 0; i < centerLines.size(); ++i)
@@ -138,23 +137,24 @@ void DrawStreet::drawStreets(Polygon1 &polygon1)
 //        Line line = centerLines[i];
 //
 //        Point current(line.getX1() , line.getY1());
+//        step2 = 30;
 //
 //        while (true)
 //        {
 //            Point destination = {line.getX2() , line.getY2()};
 //
-//            Point next1 = PolygonHelper::getNextPoint(current , destination , step1);
+//            Point next1 = PolygonHelper::getNextPoint(current , destination , step2);
 //
 //            if (next1 == destination){
 //                break;
 //            }
+//            bool foundIntersection = false;
 //
-//            Point next1UP = {next1.getX() , next1.getY() + height};
+//            Point next1UP = {next1.getX() , next1.getY() - height};
 //
 //            Line nextLine (next1.getX() , next1.getY() , next1UP.getX() , next1UP.getY());
 //
-//            bool foundIntersection = false;
-//            for(auto &upLine : topPoints)
+//            for(auto &upLine : bottomPoints)
 //            {
 //                Point intersection = PolygonHelper::getIntersectionPoint(upLine , nextLine);
 //
@@ -165,6 +165,7 @@ void DrawStreet::drawStreets(Polygon1 &polygon1)
 //                    break;
 //                }
 //            }
+//
 //            if (!foundIntersection)
 //            {
 //                for(auto &pLine : polygonLines)
@@ -185,11 +186,12 @@ void DrawStreet::drawStreets(Polygon1 &polygon1)
 //                break;
 //            }
 //
-//            Point next2UP = {next2.getX() , next2.getY() + height};
+//            Point next2UP = {next2.getX() , next2.getY() - height};
+//
+//            nextLine = Line(next2.getX() , next2.getY() , next2UP.getX() , next2UP.getY());
 //
 //            foundIntersection = false;
-//            nextLine = Line(next2.getX() , next2.getY() , next2UP.getX() , next2UP.getY());
-//            for(auto &upLine : topPoints)
+//            for(auto &upLine : bottomPoints)
 //            {
 //                Point intersection = PolygonHelper::getIntersectionPoint(upLine , nextLine);
 //
@@ -200,6 +202,7 @@ void DrawStreet::drawStreets(Polygon1 &polygon1)
 //                    break;
 //                }
 //            }
+//
 //            if (!foundIntersection)
 //            {
 //                for(auto &pLine : polygonLines)
@@ -222,107 +225,10 @@ void DrawStreet::drawStreets(Polygon1 &polygon1)
 //
 //            streets.push_back(homeLines);
 //
+//            step2 = 20;
 //            current = next2;
 //        }
 //    }
-
-    for (int i = 0; i < centerLines.size(); ++i)
-    {
-        Line line = centerLines[i];
-
-        Point current(line.getX1() , line.getY1());
-        step2 = 30;
-
-        while (true)
-        {
-            Point destination = {line.getX2() , line.getY2()};
-
-            Point next1 = PolygonHelper::getNextPoint(current , destination , step2);
-
-            if (next1 == destination){
-                break;
-            }
-            bool foundIntersection = false;
-
-            Point next1UP = {next1.getX() , next1.getY() - height};
-
-            Line nextLine (next1.getX() , next1.getY() , next1UP.getX() , next1UP.getY());
-
-            for(auto &upLine : bottomPoints)
-            {
-                Point intersection = PolygonHelper::getIntersectionPoint(upLine , nextLine);
-
-                if (intersection.getX() != INT_MAX)
-                {
-                    foundIntersection = true;
-                    next1UP = intersection;
-                    break;
-                }
-            }
-
-            if (!foundIntersection)
-            {
-                for(auto &pLine : polygonLines)
-                {
-                    Point intersection = PolygonHelper::getIntersectionPoint(pLine , nextLine);
-
-                    if (intersection.getX() != INT_MAX)
-                    {
-                        next1UP = intersection;
-                        break;
-                    }
-                }
-            }
-
-            Point next2 = PolygonHelper::getNextPoint(next1 , destination , step1);
-
-            if (next2 == destination){
-                break;
-            }
-
-            Point next2UP = {next2.getX() , next2.getY() - height};
-
-            nextLine = Line(next2.getX() , next2.getY() , next2UP.getX() , next2UP.getY());
-
-            foundIntersection = false;
-            for(auto &upLine : bottomPoints)
-            {
-                Point intersection = PolygonHelper::getIntersectionPoint(upLine , nextLine);
-
-                if (intersection.getX() != INT_MAX)
-                {
-                    foundIntersection = true;
-                    next2UP = intersection;
-                    break;
-                }
-            }
-
-            if (!foundIntersection)
-            {
-                for(auto &pLine : polygonLines)
-                {
-                    Point intersection = PolygonHelper::getIntersectionPoint(pLine , nextLine);
-
-                    if (intersection.getX() != INT_MAX)
-                    {
-                        next2UP = intersection;
-                        break;
-                    }
-                }
-            }
-
-            vector<Line> homeLines;
-            homeLines.emplace_back(next1.getX() , next1.getY() , next2.getX() , next2.getY());
-            homeLines.emplace_back(next1.getX() , next1.getY() , next1UP.getX() , next1UP.getY());
-            homeLines.emplace_back(next2.getX() , next2.getY() , next2UP.getX() , next2UP.getY());
-            homeLines.emplace_back(next1UP.getX() , next1UP.getY() , next2UP.getX() , next2UP.getY());
-
-            streets.push_back(homeLines);
-
-            step2 = 20;
-            current = next2;
-        }
-    }
 
 //    streets.push_back(topPoints);
 //    streets.push_back(bottomPoints);
