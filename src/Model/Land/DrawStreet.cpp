91145e42a746ed6f9a670f21a561d530ce1186e2
Ahmed ibarhim <aibrahim@tmcmf.com>
2025-03-11 23:15:50 +0200

    uu

--- src/Model/Land/DrawStreet.cpp
+++ src/Model/Land/DrawStreet.cpp
@@ -617,7 +617,7 @@ DrawStreet::drawHomeBorders(Polygon1 &polygon1, vector<Line> &streetsLinesOuter,
 //        homeLines.emplace_back(next1UP.getX() , next1UP.getY() , next2UP.getX() , next2UP.getY());
         homeLines.emplace_back(next2UP.getX() , next2UP.getY() , lastPoint.getX() , lastPoint.getY());
 
-//        homeBorderSol.insert(homeBorderSol.end() , homeLines.begin() , homeLines.end());
+        homeBorderSol.insert(homeBorderSol.end() , homeLines.begin() , homeLines.end());
     }
 
     return homeBorderSol;
