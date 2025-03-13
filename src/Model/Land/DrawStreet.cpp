2d172871d2c8863e2fd5f52f6b8abe6339db3c55
Ahmed ibarhim <aibrahim@tmcmf.com>
2025-03-14 01:21:17 +0200

    dd

--- src/Model/Land/DrawStreet.cpp
+++ src/Model/Land/DrawStreet.cpp
@@ -82,7 +82,7 @@ void DrawStreet::drawStreets(Polygon1 &polygon1)
 
     lengthC-=40;
 
-    step1 = ((lengthC) / (int)(divisions*4)) * 2;
+    step1 = ((lengthC / (((int)(divisions*4)) - 1))) * 2;
 
     cout<<"Id = "<<polygon1.getId()<<"  divisions = "<<divisions << " -- "<<divisionsB<<"  Length = "<<lengthC<<" -- New Step = "<<step1<<"\n";
 //    cout<<"Length = "<<lengthC<<" -- New Step = "<<step1<<"\n";
@@ -94,7 +94,7 @@ void DrawStreet::drawStreets(Polygon1 &polygon1)
     }
     lengthC-=40;
 
-    step1 = min(step1 , ((lengthC) / (int)(divisions*4)) * 2);
+    step1 = min(step1 , ((lengthC / (((int)(divisions*4)) - 1))) * 2);
     cout<<"Length = "<<lengthC<<" --New Step = "<<step1<<"\n";
 
 
