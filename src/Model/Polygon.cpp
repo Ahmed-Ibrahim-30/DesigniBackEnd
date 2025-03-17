410d3b36c65d1e51ab427aabf97b3c3cdcfa644b
Ahmed ibarhim <aibrahim@tmcmf.com>
2025-03-17 14:25:53 +0200

    d

--- src/Model/Polygon.cpp
+++ src/Model/Polygon.cpp
@@ -421,10 +421,10 @@ vector<Line> Polygon1::computeCentroidPerpendiculars()
         double y1 = line.getY1() , y2 = line.getY2();
         double dx = x2 - x1;
         double dy = y2 - y1;
+
+
         double d = dx * dx + dy * dy;
-
-        d*=10;
-
+        
         double t = ((centroid.getX() - x1) * dx + (centroid.getY()- y1) * dy) / d;
 
         Point p = {x1 + t * dx, y1 + t * dy};
