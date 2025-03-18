65fc78aa2780096f405209995bf87720c8e19084
Ahmed ibarhim <aibrahim@tmcmf.com>
2025-03-18 03:52:05 +0200

    gh

--- src/Model/Land/DrawStreet.cpp
+++ src/Model/Land/DrawStreet.cpp
@@ -853,8 +853,6 @@ vector<Line> DrawStreet::getCenterLines(Polygon1 &polygon, double centerLineHeig
 
     vector<Line> centerLine = polygon.computeCentroidPerpendiculars();
 
-    if (polygon.getId() == "25")return centerLine;
-
     vector<Point> points = polygon.getPoints();
     vector<Line> lines = polygon.getLines();
     int n = (int)points.size();
@@ -866,20 +864,15 @@ vector<Line> DrawStreet::getCenterLines(Polygon1 &polygon, double centerLineHeig
 
     for(auto &line : centerLine)
     {
-        Point one(line.getX1() , line.getY1());
         Point two(line.getX2() , line.getY2());
 
         for(auto &line2 : lines)
         {
             double x1 = min(line2.getX1() , line2.getX2()), x2 = max(line2.getX1() , line2.getX2());
             double y1 = min(line2.getY1() , line2.getY2()), y2 = max(line2.getY1() , line2.getY2());
-            if (one.getX() >= x1 && one.getX() <= x2 &&
-                one.getY() >= y1 && one.getY() <= y2)
-                continue;
 
-            if (two.getX() >= x1 && two.getX() <= x2 &&
-                two.getY() >= y1 && two.getY() <= y2)
-                continue;
+            if (PolygonHelper::isPointOnLine(two , line2))continue;
+
             Point intersectionPoint = PolygonHelper::getIntersectionPoint(line , line2);
             if (intersectionPoint.getX() != INT_MAX)
             {
