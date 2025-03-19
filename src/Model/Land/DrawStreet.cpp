7dd0e992b70beaf8a945589e2e8ebb41717d6729
Ahmed ibarhim <aibrahim@tmcmf.com>
2025-03-19 09:06:59 +0200

    gh

--- src/Model/Land/DrawStreet.cpp
+++ src/Model/Land/DrawStreet.cpp
@@ -197,6 +197,7 @@ void DrawStreet::drawSide1Streets(const vector<Line> &polygonLines,const vector<
             else
             {
                 cout<<"cur = "<<cur.getX() <<" "<<cur.getY()<<"\n";
+                cout<<"slopes[i] = "<<slopes[i]<<"\n";
                 Point second = PolygonHelper::getSecondLinePoint(cur , slopes[i] , 1000*opp);
                 cuttingLine.setX2(second.getX());
                 cuttingLine.setY2(second.getY());
