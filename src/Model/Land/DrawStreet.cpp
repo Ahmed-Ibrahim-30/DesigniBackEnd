d1d6c2a51090aebc23b72204b36ee03f2313a6f3
Ahmed ibarhim <aibrahim@tmcmf.com>
2025-03-19 08:30:59 +0200

    gh

--- src/Model/Land/DrawStreet.cpp
+++ src/Model/Land/DrawStreet.cpp
@@ -220,7 +220,7 @@ void DrawStreet::drawSide1Streets(const vector<Line> &polygonLines,const vector<
 
 
         // next1UP , next2UP
-        Polygon1 outerStreet({startPoint1 });
+        Polygon1 outerStreet({startPoint1 , {0,0}});
 
         Line first (last.getX() , last.getY() , startPoint1.getX() , startPoint1.getY());
         Line second (lastPoint.getX() , lastPoint.getY() , startPoint1.getX() , startPoint1.getY());
