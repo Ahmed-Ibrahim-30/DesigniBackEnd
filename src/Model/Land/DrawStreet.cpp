3b0df0d6d0d189409d46ae8573e0bbd2e51ce96a
Ahmed ibarhim <aibrahim@tmcmf.com>
2025-03-21 14:21:24 +0200

    fix

--- src/Model/Land/DrawStreet.cpp
+++ src/Model/Land/DrawStreet.cpp
@@ -728,7 +728,7 @@ Point DrawStreet::getNextPoint(const Point &start, int &lineIndex, const vector<
     Point endPoint , startPoint = start;
     double mainStep = step;
     step = (int)(step*10);
-    step/10;
+    step/=10;
     while (true)
     {
         const Line& line = lines[lineIndex];
