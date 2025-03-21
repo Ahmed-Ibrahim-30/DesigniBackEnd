77122fb08dd3454b7f038e7c2fc9c98c73edfdd7
Ahmed ibarhim <aibrahim@tmcmf.com>
2025-03-21 17:50:12 +0200

    fix

--- src/Model/Land/DrawStreet.cpp
+++ src/Model/Land/DrawStreet.cpp
@@ -547,7 +547,7 @@ DrawStreet::drawHomeBorders(Polygon1 &polygon1, vector<Line> &streetsLinesOuter,
             Polygon1 homeLand = getHomePolygon(startH , endH ,startH1 ,  endH1 ,polygonLines , polygon1 , bottomLines);
 
 
-            Land land(polygon1);
+            Land land(homeLand);
             vector<Polygon1> ans;
             ans = land.SplitLand({1,1}  , static_cast<LandDivisionSortingStrategy>(0));
 
