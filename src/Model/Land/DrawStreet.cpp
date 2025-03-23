2923d6f9bb2af032b2f3bf235727638ea6852d5a
Ahmed ibarhim <aibrahim@tmcmf.com>
2025-03-23 02:07:00 +0200

    fix

--- src/Model/Land/DrawStreet.cpp
+++ src/Model/Land/DrawStreet.cpp
@@ -65,7 +65,7 @@ void DrawStreet::drawStreets(Polygon1 &polygon1)
     startSpace = step1 / 2;
     drawSide1Streets(polygonLines ,centerLinesTop ,  spacingLines , step1 , divisions);
     startSpace = step1;
-    drawSide1Streets(polygonLines ,centerLinesBottom ,  spacingLines , step1 , divisions);
+//    drawSide1Streets(polygonLines ,centerLinesBottom ,  spacingLines , step1 , divisions);
 //    vector<vector<Line>> bottomStreets = drawBottomStreets(polygonLines ,centerLinesBottom, spacingLines , step1 , divisions);
 
 
