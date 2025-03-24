723459ffbcba3c762111fc0ab2feda6f3659ddbb
Ahmed ibarhim <aibrahim@tmcmf.com>
2025-03-24 17:16:36 +0200

    fix

--- src/Model/Land/DrawStreet.cpp
+++ src/Model/Land/DrawStreet.cpp
@@ -63,9 +63,9 @@ void DrawStreet::drawStreets(Polygon1 &polygon1)
     cout<<"Length = "<<lengthC<<" --New Step = "<<step1<<"\n";
 
     startSpace = step1 / 2;
-    drawSide1Streets(polygonLines ,centerLinesTop ,  spacingLines , step1 , divisions);
+//    drawSide1Streets(polygonLines ,centerLinesTop ,  spacingLines , step1 , divisions);
     startSpace = step1;
-//    drawSide1Streets(polygonLines ,centerLinesBottom ,  spacingLines , step1 , divisions);
+    drawSide1Streets(polygonLines ,centerLinesBottom ,  spacingLines , step1 , divisions);
 
     for(auto &city : cities)
     {
@@ -148,7 +148,7 @@ void DrawStreet::drawSide1Streets(const vector<Line> &polygonLines,const vector<
 
     for (int m = 0; m < divisions; ++m)
     {
-//        if (m==3)break;
+        if (m==1)break;
         double newStep = !m ? startSpace : step;
 
         Point last = lastPoint;
