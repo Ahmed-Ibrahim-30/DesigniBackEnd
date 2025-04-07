1060c252d50ead781624ff2d6aecc392962dec24
Ahmed ibarhim <aibrahim@tmcmf.com>
2025-04-07 13:31:39 +0200

    add logs

--- src/Controller/APIController.cpp
+++ src/Controller/APIController.cpp
@@ -123,7 +123,7 @@ void APIController::templateRoutes(crow::SimpleApp &app)
 
         vector<pair<string , string>> conn {
                 {"Bedroom1", "Bedroom2"},
-                {"Bath1", "Bedroom2"},
+//                {"Bath1", "Bedroom2"},
 //                {"Bath1", "Bath2"},
                 {"Dining", "Bedroom5"},
                 {"Kitchen", "GBath"},
