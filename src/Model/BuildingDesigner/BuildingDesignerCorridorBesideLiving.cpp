df1a36714c824895804dca5bc517a42d07db1068
Ahmed ibarhim <aibrahim@tmcmf.com>
2025-04-13 15:42:57 +0200

    refactor

--- src/Model/BuildingDesigner/BuildingDesignerCorridorBesideLiving.cpp
+++ src/Model/BuildingDesigner/BuildingDesignerCorridorBesideLiving.cpp
@@ -358,7 +358,7 @@ vector<Design> BuildingDesignerCorridorBesideLiving::generateDesign()
     /**
      * Third Design
      */
-    YChanged = Corridor.getY2() - Living.getY2() ;
+    YChanged = Corridor.getY1() - Living.getY2() ;
     cout<< "YChanged = "<<YChanged<<"\n";
     livingCore = Design ("Living Core" , newRooms);
     livingCore.shiftDesignY(YChanged);
